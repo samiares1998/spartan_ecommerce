51dd4d4def12db7946854b20639501ea98346bd8
sami arevalo <samiarevalo@samis-MacBook-Pro.local>
2025-04-20 21:00:33 -0500

    changes7

--- resources/views/client/categoryProducts.blade.php
+++ resources/views/client/categoryProducts.blade.php
@@ -1,6 +1,6 @@
 <x-template.layout title="{{$title}}">
   <x-organisms.navbar cartCount=10 :path="$shop->path"/>
-  <x-organisms.products :dataProduct="$category->product">
+  <x-organisms.products :dataProduct="$category->products">
     <h1 class="pb-4">Categoria : {!! str_replace('-', ' ', ucwords($category->name)) !!}</h1>
   </x-organisms.products>
   <x-organisms.footer :shop="$shop"/>
